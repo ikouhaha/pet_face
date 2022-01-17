b5795dfadbbb4dc5f6f875ca321c8291e9ce7353
Dennis Au Yeung <ikouhaha888@gmail.com>
2022-01-17 14:59:19 +0800

    add download checking

--- download.py
+++ download.py
@@ -3,6 +3,10 @@ import sys
 from pathlib import Path
 
 source_path = "./source/cat"
+
+if(os.path.exists(source_path)):
+    exit
+
 Path(source_path).mkdir(parents=True, exist_ok=True)
 os.environ['KAGGLE_USERNAME'] = sys.argv[1]
 os.environ['KAGGLE_KEY'] = sys.argv[2]
