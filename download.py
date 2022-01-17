1ce34bbd866563d4874befdcd49823c77c3ab172
Dennis Au Yeung <ikouhaha888@gmail.com>
2022-01-17 16:39:53 +0800

    update download py

--- download.py
+++ download.py
@@ -3,13 +3,10 @@ import sys
 from pathlib import Path
 import shutil
 
-source_path = "./source/cat"
+source_path = "./source/cats"
 zip_path = source_path + "/cat-dataset.zip"
 
 
-
-if(os.path.exists(source_path)):
-    exit
 
 Path(source_path).mkdir(parents=True, exist_ok=True)
 os.environ['KAGGLE_USERNAME'] = sys.argv[1]
@@ -19,7 +16,10 @@ import kaggle
 
 kaggle.api.authenticate()
 
-kaggle.api.dataset_download_files('crawford/cat-dataset', path=source_path, unzip=True)
+kaggle.api.dataset_download_files('crawford/cat-dataset', path=source_path, unzip=True,quiet=False,force=True)
+
+if(os.path.exists(source_path+"/cats")):
+    shutil.rmtree(source_path+"/cats",ignore_errors=True)
 
 if(os.path.exists(zip_path)):
     os.remove(zip_path)
